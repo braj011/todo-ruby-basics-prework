bd2d5a73dd758d0a6a08e7e0a5941fd8d37e687e
braj011 <bhagyaraj01@gmail.com>
2018-08-15 19:50:59 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -17,20 +17,6 @@ end
 
 
 
-  describe '#greeting' do
-    it 'takes two arguments' do
-      greeting = "Hi there, "
-      name = "Bobby!"
-
-      expect{ greeting(greeting, name) }.to_not raise_error
-    end
-
-    it 'raises an error with one argument' do
-      greeting = "Hi there, "
-
-      expect{ greeting(greeting)}.to raise_error
-    end
-  end
 
 def return_a_value
 
